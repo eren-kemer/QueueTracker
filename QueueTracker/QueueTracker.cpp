344e46080fe02aaeac627b39d5ba6b11aaa1230e
Eren Kemer <github@eren.io>
2021-02-07 00:56:42 +0100

    Initial public commit

--- QueueTracker/QueueTracker.cpp
+++ QueueTracker/QueueTracker.cpp
@@ -5,32 +5,11 @@ static const std::string savefile = "bakkesmod/data/queuetimetracker.data";
 
 BAKKESMOD_PLUGIN(QueueTracker, "Queue time tracker", "0.1", PLUGINTYPE_FREEPLAY)
 
-
-
-/*void Log(void* object, std::string message)
-{
-	auto plugin = (QueueTracker*)object;
-	plugin->cvarManager->log(message);
-}*/
-
-
 void QueueTracker::onLoad()
 {
-	queueStart_time, queueEnd_time, queueElapsed_time = time(NULL);
+	queueStart_time, queueElapsed_time = time(NULL);
 	cvarManager->log("QueueTracker loaded");
-	gameWrapper->Toast("QueueTracker", "QueueTracker loaded successfully!");
-	
-	//gameWrapper->HookEventWithCaller<ServerWrapper>(
-	//	"Function GameEvent_Soccar_TA.Active.StartRound",
-	//	bind(
-	//		&QueueTracker::EndTimer,
-	//		this,
-	//		std::placeholders::_1,
-	//		std::placeholders::_2,
-	//		std::placeholders::_3
-	//	)
-	//	);
-	
+
 	gameWrapper->HookEventWithCaller<ServerWrapper>(
 		"Function ProjectX.CheckReservation_X.StartSearch",
 		bind(
@@ -41,72 +20,36 @@ void QueueTracker::onLoad()
 			std::placeholders::_3)
 		);
 
-
-	
-/*	gameWrapper->HookEventWithCaller<ServerWrapper>(
-		"Function ProjectX.OnlineGameMatchmakingBase_X.EventFindGameComplete",
-		bind(
-			&QueueTracker::EndTimerGameComplete,
-			this,
-			std::placeholders::_1,
-			std::placeholders::_2,
-			std::placeholders::_3)
-		);
-*/
-
-
-		gameWrapper->HookEventWithCaller<ServerWrapper>(
+	gameWrapper->HookEventWithCaller<ServerWrapper>(
 		"Function TAGame.GameEvent_TA.PostBeginPlay",
 		bind(
-			&QueueTracker::EndTimerPostBeginPlay,
+			&QueueTracker::EndTimer,
 			this,
 			std::placeholders::_1,
 			std::placeholders::_2,
 			std::placeholders::_3)
 		);
-
 }
 
 void QueueTracker::onUnload()
 {
 	cvarManager->log("QueueTracker unloaded");
-	gameWrapper->Toast("QueueTracker", "QueueTracker unloaded successfully!");
 }
 
 void QueueTracker::StartTimer(ServerWrapper caller, void* params, std::string eventName) {
 
 	queueStart_time = time(NULL);
 	cvarManager->log("Queue time logged!");
-	gameWrapper->Toast("QueueTracker", "Queue timer started!");
 }
 
-
-/*void QueueTracker::EndTimerGameComplete(ServerWrapper caller, void* params, std::string eventName) {
-	auto mmrWrapper = gameWrapper->GetMMRWrapper();
-	auto playlist = mmrWrapper.GetCurrentPlaylist();
-	auto playlistName = GetPlaylistName(playlist);
-
-	//if (playlistName != "Queueless" && playlistName != "") {
-		queueEnd_time = time(NULL);
-		queueElapsed_time = queueEnd_time - queueStart_time;
-		queueElapsed_str = std::to_string(queueElapsed_time);
-
-		cvarManager->log("Queue timer finished in " + std::to_string(queueElapsed_time) + " seconds.");
-		gameWrapper->Toast("QueueTracker", ("GameCompleteMatch found in " + queueElapsed_str + " seconds!"));
-		gameWrapper->LogToChatbox("Test" + queueElapsed_str);
-	//}
-	
-}*/
-
-
-void QueueTracker::EndTimerPostBeginPlay(ServerWrapper caller, void* params, std::string eventName) {
+void QueueTracker::EndTimer(ServerWrapper caller, void* params, std::string eventName) {
 	auto mmrWrapper = gameWrapper->GetMMRWrapper();
 	auto playlist = mmrWrapper.GetCurrentPlaylist();
 	auto playlistName = GetPlaylistName(playlist);
 
 	if (playlistName != "Queueless" && playlistName != "") {
-		queueEnd_time = time(NULL);
-		queueElapsed_time = queueEnd_time - queueStart_time;
+		queueElapsed_time = time(NULL) - queueStart_time;
+
 		if (queueElapsed_time >= 60) {
 			queueElapsed_str = std::to_string(queueElapsed_time / 60) + " minute(s)!";
 		}
@@ -119,8 +62,7 @@ void QueueTracker::EndTimerPostBeginPlay(ServerWrapper caller, void* params, std
 	}
 }
 
-
-std::string GetPlaylistName(int playlistId) {
+std::string QueueTracker::GetPlaylistName(int playlistId) {
 	switch (playlistId) {
 	case(1):
 		return "Duel";
--- QueueTracker/QueueTracker.h
+++ QueueTracker/QueueTracker.h
@@ -1,8 +1,7 @@
 #pragma once
 #pragma comment( lib, "pluginsdk.lib" )
 #include "bakkesmod/plugin/bakkesmodplugin.h"
-#include "bakkesmod\wrappers\includes.h"
-#include "utils/parser.h"
+
 #include <string>
 #include <ctime>
 
@@ -13,19 +12,12 @@ class QueueTracker : public BakkesMod::Plugin::BakkesModPlugin
 {
 private:
 
+	void EndTimer(ServerWrapper caller, void* params, std::string eventName);
+	void StartTimer(ServerWrapper caller, void* params, std::string eventName);
+	std::string GetPlaylistName(int playlistId);
 public:
 	virtual void onLoad();
 	virtual void onUnload();
-
-	time_t queueStart_time, queueEnd_time, queueElapsed_time;
+	time_t queueStart_time, queueElapsed_time;
 	std::string queueElapsed_str;
-	void EndTimer(ServerWrapper caller, void* params, std::string eventName);
-	void StartTimer(ServerWrapper caller, void* params, std::string eventName);
-	void EndTimerGameComplete(ServerWrapper caller, void* params, std::string eventName);
-	void EndTimerPostBeginPlay(ServerWrapper caller, void* params, std::string eventName);
-
 };
-
-std::string GetPlaylistName(int playlistId);
-
-//std::shared_ptr<bool> showNotifications = std::make_shared<bool>(true);
